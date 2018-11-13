c263a07424b09be8421bc2cbe977201c923d2ff2
Vandana Saxena <vansaxena@yahoo.com>
2018-11-13 18:58:45 -0500

    Updates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-<!---food.html time calculation---  >
+/*---food time calculation---  */
 function showDate(){
   document.getElementById('demo').innerHTML = Date()}
-
+/*---appetizer to pop up message-----*/
   function myFunction() {
       alert("Sorry!! Appetizer is not available .")}
